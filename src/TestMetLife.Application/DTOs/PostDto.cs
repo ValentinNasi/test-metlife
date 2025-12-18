bfaafcdbd1aaf628185c861f84cc5a5a70b5c738
gm_vnasi <vnasi@gmigliarino.com>
2025-12-18 17:56:26 -0300

    add entities, dtos, and interfaces for posts

---
+++ src/TestMetLife.Application/DTOs/PostDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TestMetLife.Application.DTOs;
+
+public class PostDto
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Body { get; set; } = string.Empty;
+}
